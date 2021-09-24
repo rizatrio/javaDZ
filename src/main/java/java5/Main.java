52a68de61ab9e9b7a5bc1311899e0f1c148da91f
Rizat19 <e-rizat.orazalina@homecredit.kz>
2021-09-24 14:10:30 +0600

    lessonFive Homework

--- src/main/java/java5/Main.java
+++ src/main/java/java5/Main.java
@@ -2,6 +2,18 @@ package java5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("DZ 5");
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Popov Alexandr", "TeamLead", "popov89@mailbox.com", "87074123658", 100000, 32);
+        employees[1] = new Employee("Ivanov Ivan", "Analyst", "ivivan@mailbox.com", "87770258963", 50000, 47);
+        employees[2] = new Employee("Likhachev Andrey", "Engineer", "liandrey@mailbox.com", "87052103678", 45000, 22);
+        employees[3] = new Employee("Perekrestov Denis", "Developer", "denis00@mailbox.com", "87477894561", 80000, 29);
+        employees[4] = new Employee("Budon Olga", "Engineer", "olgaolga@mailbox.com", "87019424296", 75000, 42);
+
+        System.out.println("Сотрудники старше 40 лет: ");
+        for (Employee employee : employees) {
+            if (employee.getAge() > 40) {
+                System.out.println(employee);
+            }
+        }
     }
 }
